ca7a446b0c208a7f2eb8c3c01019ee90040e7d6d
Valntinka <vorobei7242@yandex.ru>
2015-12-30 00:06:48 +0400

    change launch for not only jar tools

--- py/launch.py
+++ py/launch.py
@@ -13,18 +13,22 @@ def rewrite(file_name):
 
 
 def launch(comm, result_dir):
-    time = datetime.datetime.now().strftime("%H_%M_%S")
+    t1 = datetime.datetime.now()
+    time = t1.strftime("%H_%M_%S")
     result_file = os.path.join(result_dir, '{}.txt'.format(time))
     with open(result_file, "a+") as r_f:
-       r_f.write("{}\n\n".format(comm[-1]))
+        r_f.write("{}\n\n".format(comm.split(" ")[-1]))
 #    rewrite(comm[-1])
-    comm += ['>>', result_file, '2>&1']
-    os.system(" ".join(comm))
+    comm += '>> {} 2>&1'.format(result_file)
+    os.system(comm)
+    t2 = datetime.datetime.now()
+    with open(result_file, "a+") as r_f:
+        r_f.write("\n\nRuntime: {}".format(t2 - t1))
 
 
-def main(tool, dir_name, options=[]):
+def main(cmd, dir_name, result_tag):
     time = datetime.datetime.now().strftime("%Y_%m_%d")
-    result_dir = os.path.join("../results_{}".format(tool), time)
+    result_dir = os.path.join("../results_{}".format(result_tag), time)
     if not os.path.exists(result_dir):
         os.makedirs(result_dir)
     for root, folders, files in os.walk(dir_name):
@@ -33,15 +37,12 @@ def main(tool, dir_name, options=[]):
             print(file)
             file = file.replace(" ", "\ ")
             try:
-                launch(['java', '-jar', '-Xmx8G', '{}.jar'.format(tool)] + options + [file], result_dir)
+                cmd += " " + file
+                launch(cmd, result_dir)
             except Exception as exc:
                 print("some exception")
                 print(exc)
-#            exit(0)
 
 
 if __name__ == "__main__":
-    if sys.argv[1] == "hybroscale_1.3":
-        main(sys.argv[1], sys.argv[2], ['-mode', 'hNum', '-i'])
-    else:
-        main(sys.argv[1], sys.argv[2])
+    main(sys.argv[1], sys.argv[2], sys.argv[3])
