478b39a1dd63edf2779f9de1195370c6cdd84f18
Lipen <lipen00@gmail.com>
2017-07-21 01:23:06 +0300

    Plot only n+k<20

--- analyze.R
+++ analyze.R
@@ -5,7 +5,7 @@ data$n <- as.numeric(data$n)
 data$k <- as.numeric(data$k)
 data$t <- as.numeric(data$t)
 
-ggplot(subset(data), aes(n + k, t, group=sat)) +
+ggplot(subset(data, n+k < 20), aes(n + k, t, group=sat)) +
   geom_point(aes(colour=sat))
 
 library(rgl)
