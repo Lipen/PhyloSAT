90406d6e068a067580d3719d5b6a8d9a50293975
Lipen <lipen00@gmail.com>
2017-09-22 16:27:07 +0300

    Add latex table output (old)

--- meh_compthing.py
+++ meh_compthing.py
@@ -1,4 +1,3 @@
-import sys
 import pandas
 import numpy as np
 
@@ -9,44 +8,52 @@ filepath_old = 'oldthing.log'
 def get_data(filepath):
     df = pandas.read_csv(filepath, dtype={'n': np.int32, 'k': np.int32, 't': np.float64})
     data = {}
+    dataStd = {}
     for f in df.f.unique():
         xx = df[df.f == f]
-        assert all(xx.n.iloc[i] == xx.n.iloc[0] for i in range(len(xx.n)))
-        assert all(xx.k.iloc[i] == xx.k.iloc[0] for i in range(len(xx.k)))
+        assert all(xx.n.iloc[i] == xx.n.iloc[0] for i in range(len(xx.n))), "unequality over ns"
+        assert all(xx.k.iloc[i] == xx.k.iloc[0] for i in range(len(xx.k))), "unequality over ks"
         data[xx.f.iloc[0]] = (xx.n.iloc[0], xx.k.iloc[0], xx.t.mean())
-    return data
+        dataStd[xx.f.iloc[0]] = xx.t.std()
+    return data, dataStd
 
 
 def main():
-    data_new = get_data(filepath_new)
-    data_old = get_data(filepath_old)
+    data_new, dataStd_new = get_data(filepath_new)
+    data_old, dataStd_old = get_data(filepath_old)
 
     names = sorted(set.union(set(data_new), set(data_old)))
-    ll = max(len('name'), max(len(f) for f in data_new), max(len(f) for f in data_old))
-    print('[*] {1: ^{0}} | n_new | n_old | k_new | k_old | time_new | time_old'.format(ll, 'name'))
+    # ll = max(len('name'), max(len(f) for f in data_new), max(len(f) for f in data_old))
+    # print('[*] {1: ^{0}} | n_old | n_new | k_old | k_new | time_old | time_new'.format(ll, 'name'))
     for name in names:
         if name in data_new:
             n_new, k_new, t_new = data_new[name]
-            n_new = '{: ^5}'.format(n_new)
-            k_new = '{: ^5}'.format(k_new)
-            t_new = '{: >7.3f}s'.format(t_new)
+            # n_new = '{: ^5}'.format(n_new)
+            # k_new = '{: ^5}'.format(k_new)
+            # t_new = '{:.3f}s'.format(t_new)
         else:
-            n_new = ' --- '
-            k_new = ' --- '
-            t_new = ' -------'
+            # n_new = ' --- '
+            # k_new = ' --- '
+            # t_new = ' -------'
+            n_new = k_new = t_new = '--'
 
         if name in data_old:
             n_old, k_old, t_old = data_old[name]
-            n_old = '{: ^5}'.format(n_old)
-            k_old = '{: ^5}'.format(k_old)
-            t_old = '{:.3f}s'.format(t_old)
+            # n_old = '{: ^5}'.format(n_old)
+            # k_old = '{: ^5}'.format(k_old)
+            # t_old = '{: >7.3f}s'.format(t_old)
         else:
-            n_old = ' --- '
-            k_old = ' --- '
-            t_old = '--------'
+            # n_old = ' --- '
+            # k_old = ' --- '
+            # t_old = '--------'
+            n_old = k_old = t_old = '--'
 
-        f = '{1: ^{0}}'.format(ll, name)
-        print(' >  ' + ' | '.join([f, n_new, n_old, k_new, k_old, t_new, t_old]))
+        # f = '{1: ^{0}}'.format(ll, name)
+        # print(' >  ' + ' | '.join([f, n_old, n_new, k_old, k_new, t_old, t_new]))
+        if n_new != n_old or k_new != k_old:
+            # print('[!] Skipping {}'.format(name))
+            continue
+        print('\t\\texttt{{{}}} & {} & {} & {:.3f} \\pm {:.2f} & {:.3f} \\pm {:.2f} \\\\'.format(name.replace('_', '\\_'), n_new, k_new, t_old, dataStd_old[name], t_new, dataStd_new[name]))
 
 
 if __name__ == '__main__':
